d3f9f774ac723d9c8c049c2e1c156a057ca7bf01
Justin Rasnic <jrasnic@fullsail.edu>
2012-08-07 17:24:25 -0400

    remove select element from html and created JS file
    removed select element to prepare for dynamic creation in JS

---
+++ main.js
@@ -0,0 +1,11 @@
+// Justin Rasnic
+// VFW 1208
+// Project 2
+
+// Delay JS until DOM is ready (all code must be inside this function)
+
+// get Element shortcut
+
+// variables
+
+// set click events
