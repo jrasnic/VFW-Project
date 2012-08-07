ef172d4df33e530948ac393c4367556d2fb4dbe2
Justin Rasnic <jrasnic@fullsail.edu>
2012-08-07 17:58:43 -0400

    initial JS prep coding and minor html changes

--- main.js
+++ main.js
@@ -3,9 +3,23 @@
 // Project 2
 
 // Delay JS until DOM is ready (all code must be inside this function)
+window.addEventListener("DOMContentLoaded", function(){
 
-// get Element shortcut
+	// get Element shortcut
+	function $(x){
+		var element = document.getElementById(x);
+		return element;
+	};
 
-// variables
+	// variables
+	var genres = ["Action-Adventure", "Fighting", "FPS", "Platformer", "Puzzle", "RPG", "Simulation", "Sports", "Strategy"];
 
-// set click events
+	// set click events
+	/*var displayLink = $("display");
+	displayLink.addEventListener("click", getData);
+	var clearLink = $("clear");
+	clearLink.addEventListener("click", clearData);
+	var save = $("submit");
+	save.addEventListener("click", saveData);*/
+
+});
